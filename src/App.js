511e69a7306634a4b7f1892b7df8cf3967cef65f
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-11-18 16:33:26 -0300

    Configura eslint e prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <div>ata</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div>ata</div>
+        </div>
+    )
 }
 
-export default App;
+export default App
