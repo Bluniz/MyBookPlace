2b6c29973f9272e0cd7788e76030f05386a9a93d
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-11-19 21:31:01 -0300

    Remove tag de restrição da URL

--- src/service/booksService.js
+++ src/service/booksService.js
@@ -2,7 +2,7 @@ import api from './api';
 import bookActions from '../actions/bookActions';
 
 export function getBooks(term, dispatch, index){
-  return api.get(`/volumes?q=${term}&startIndex=${index}&langRestrict=Portugues`).then(response=>{
+  return api.get(`/volumes?q=${term}&startIndex=${index}`).then(response=>{
     console.log(response)
     if(response === undefined || response.data.totalItems === 0){
       dispatch(bookActions.notFoundBooks(true))
