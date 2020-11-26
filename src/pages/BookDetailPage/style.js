7597ea31923ad2df715d1ebe16e833a99aa235f6
Lucas Rosa <lucassantosrosa51@gmail.com>
2020-11-25 23:42:01 -0300

    Corrige styles da detailBook

--- src/pages/BookDetailPage/style.js
+++ src/pages/BookDetailPage/style.js
@@ -1,12 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles(() => ({
     root: {
-        height: '100vh',
         background: '#f5f5f5',
-        display: 'flex',
-        flexDirection: 'column',
         color: '#122230',
-        width: '100vw',
     },
 
     dontOverflow: {
