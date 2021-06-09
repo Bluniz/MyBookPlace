9928286d2a88b9827589bc2963e76d321f94457d
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-06-09 07:40:36 -0300

    add filterButtonData object controll

---
+++ src/helper/index.js
@@ -0,0 +1,12 @@
+export const filterButtonsData = [
+    {
+        id: 0,
+        value: '&orderBy=newest',
+        text: 'Mais novo',
+    },
+    {
+        id: 1,
+        value: '&orderBy=relevance',
+        text: 'Relevância',
+    },
+]
