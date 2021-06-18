ee362f724e6674de610a89c01a4b9ddc1f6cab1b
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-06-18 16:29:17 -0300

    Add tests of BookCard Component

---
+++ src/components/BookCard/BookCard.spec.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import BookCard from '.'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
+const mockedClasses = {
+    root: '',
+    container: '',
+    image: '',
+    subheader: '',
+    buttonAndRatingContainer: '',
+    button: '',
+}
+
+const mockedBook = {
+    bookId: 0,
+    title: 'Meu Livro',
+    subtitle: 'Meu subtitulo',
+    rating: 0,
+    image:
+        'https://images.unsplash.com/photo-1593642532871-8b12e02d091c?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+    classes: mockedClasses,
+}
+
+describe('BookCard Component', () => {
+    it('renders correctly', () => {
+        const history = createMemoryHistory()
+
+        render(
+            <Router history={history}>
+                <BookCard
+                    bookId={mockedBook.id}
+                    title={mockedBook.title}
+                    subtitle={mockedBook.subtitle}
+                    rating={mockedBook.rating}
+                    image={mockedBook.image}
+                    classes={mockedBook.classes}
+                />
+            </Router>
+        )
+
+        expect(screen.getByText('Meu Livro')).toBeInTheDocument()
+        expect(screen.getByText('Meu subtitulo')).toBeInTheDocument()
+        expect(screen.getByText('Ver mais')).toBeInTheDocument()
+    })
+})
